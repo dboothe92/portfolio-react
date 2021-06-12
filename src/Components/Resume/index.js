ab6bc7fa987fdb0027f38888b703f743814d960c
Dan B <dbraunby@hotmail.com>
2021-06-11 21:44:27 -0600

    Resume page content

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -3,7 +3,75 @@ import React from 'react';
 const Resume = () => {
     return(
         <section>
-            <h1>Resume</h1>
+            <h3>Full-Stack Developer</h3>
+            <div>
+                <h4>Summary of Qualifications</h4>
+                <p>
+                    Recent graduate of Davis Technical College Software Development program. 
+                    Driven professional with proven record of building relationships, managing 
+                    projects, and coaching individuals to success. Adaptive leader with the 
+                    ability to work independently or function highly as a member of a team. 
+                    Skilled with HTML, CSS, ASP.NET, Git, Java, JavaScript, C#, and C++. 
+                    Recognized for being level-headed while utilizing critical thinking and 
+                    complex problem solving. Skilled at organization and attention to detail. 
+                </p>
+            </div>
+
+            <div>
+                <h4>Competencies</h4>
+                <ul>
+                    <li>Experience with object-oriented language Javascript</li>
+                    <li>Familiar with web development using the MERN stack</li>
+                    <li>Able to work as member of a team on various software development-related projects</li>
+                    <li>Follows new developments in industry practices and technologies</li>
+                    <li>Constructs concise plans in order to work through problems quickly and efficiently</li>
+                    <li>Ability to manage and organize time in order to perform multiple tasks effectively</li>
+                </ul>
+            </div>
+
+            <div>
+                <h4>Education</h4>
+                <div>
+                    <p>University of Utah Bootcamp<span>2020</span></p>
+                    <p><i>Software Development</i></p>
+                </div>
+            </div>
+
+            <div>
+                <h4>Experience</h4>
+                <div>
+                    <h5>Signs.com<span>2019-Present</span></h5>
+                    <div>
+                        <p><i>Customer Experience Lead<span>2020-2021</span></i></p>
+                        <p>
+                            Responsible for ensuring customer satisfaction. Work as a team and 
+                            individual to increase company revenue and rating. Responsible for 
+                            10% of department revenue in a team of 20 for 2 consecutive record 
+                            breaking months. 
+                        </p>
+                    </div>
+                    <div>
+                        <p><i>Customer Experience Representative</i></p>
+                        <p>
+                            Responsible for ensuring customer satisfaction. Work as a team and 
+                            individual to increase company revenue and rating. Responsible for 
+                            10% of department revenue in a team of 20 for 2 consecutive record 
+                            breaking months. 
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h5>El Matador<span>2009-2019</span></h5>
+                    <p><i>Manager<span>2016-2019</span></i></p>
+                    <p>
+                        Handled all day to day operations. Fostered strong relationships with 
+                        employees and customers. Worked with each department to create a focus 
+                        of teamwork and service. Implemented more efficient policies to expedite 
+                        the flow of the business. Created training manual. Trained new employees 
+                        in all positions. 
+                    </p>
+                </div>
+            </div>
         </section>
     );
 };
